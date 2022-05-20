baf8936a47b468a6eb8daf750c76f52ff700ac33
Matheus Barnabe Pereira <matheusbp88@gmail.com>
2022-05-20 18:14:21 -0300

    apostila08-redux: fim da secao 2.8

--- index.js
+++ index.js
@@ -31,3 +31,31 @@ const solicitarCashBack = (nome, valor) => {
         }
     }
 }
+
+//essa é uma reducer
+const historicoDePedidosCashbackReducer = (historicoDePedidosCashbackAtual = [], acao) => {
+    if (acao.type == "SOLICITAR_CASHBACK"){
+        return [
+            ...historicoDePedidosCashbackAtual,
+            acao.payload
+        ]
+    }
+    return historicoDePedidosCashbackAtual
+}
+
+//essa é uma outra função reducer
+const caixaReducer = (dinheiroEmCaixa = 0, acao) => {
+    //se a acao for SOLICITAR_CASHBACK, reduzir o valor do caixa
+    if (acao.type == "SOLICITAR_CASHBACK"){
+        dinheiroEmCaixa -= acao.payload.valor
+    } 
+    //senao se a acao for CRIAR_CONTRATO, aumentare o valor do caixa
+    else if (acao.type == "CRIAR_CONTRATO"){
+        dinheiroEmCaixa += acao.payload.taxa
+    }
+    //senao, somente devolver a fatia de estado envolvido
+    return dinheiroEmCaixa
+}
+
+
+
